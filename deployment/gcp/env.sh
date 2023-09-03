ea59d1060476d17234c185e215110df853c091ff
Matar7 <matar@linux.com>
2023-09-03 16:03:05 +0300

    fix gcp deployment

--- deployment/gcp/env.sh
+++ deployment/gcp/env.sh
@@ -6,3 +6,9 @@ export CDAP_ENDPOINT=$(gcloud beta data-fusion instances describe \
     --location=$REGION \
     --format="value(apiEndpoint)" \
   ${INSTANCE_ID})
+
+#export GCP_PROJECT_NUMBER="$(gcloud projects describe $(gcloud config get-value project) --format='value(projectNumber)')"
+
+#export REPOSITORY="samples"
+
+#printenv | grep GCP_PROJECT_ID
