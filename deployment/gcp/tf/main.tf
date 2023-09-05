25150a79dcaeb415aa18f74c8be4b6389a3ec04c
Matar7 <matar@linux.com>
2023-09-05 15:49:30 +0300

    update TF deployment

--- deployment/gcp/tf/main.tf
+++ deployment/gcp/tf/main.tf
@@ -3,16 +3,16 @@ module "service_account" {
   version    = "4.2.1"
   project_id = var.project_id
   prefix     = "sa-cloud-run"
-  names      = ["simple"]
+  names      = ["pymovies"]
 }
 
 module "cloud_run" {
   source                = "GoogleCloudPlatform/cloud-run/google"
   version               = "0.9.1"
-  service_name          = "ci-cloud-run"
+  service_name          = "pymovies"
   project_id            = var.project_id
   location              = "us-central1"
-  image                 = "gcr.io/golang-389808/pymovies"
+  image                 = var.image_id
   service_account_email = module.service_account.email
   members               = ["allUsers"]
 }
--- deployment/gcp/tf/variables.tf
+++ deployment/gcp/tf/variables.tf
@@ -2,3 +2,8 @@ variable "project_id" {
   description = "The project ID to deploy to"
   type        = string
 }
+
+variable "image_id" {
+  description = "The image ID to be deplied"
+  type = string
+}
